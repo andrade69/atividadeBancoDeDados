fede8b5cea2328f040325b1809b14ad26d08d426
Gabriel-Andrade <165605391+andrade69@users.noreply.github.com>
2025-09-04 01:19:16 +0000

    Adicionadas as inserções

--- schema.sql
+++ schema.sql
@@ -35,4 +35,23 @@ status TEXT NOT NULL (status ('pago', 'pendente'),
 FOREIGN KEY (id_inscricao) REFERENCES isncricao(id) ON DELETE CASCADE
 );
 
+INSERT INTO participante(id, nome, email, telefone) VALUES 
+(1, 'Leticia', 'exemploEmail1@gmail.com', '00000000000'),
+(2, 'Gabriel', 'exemploEmail2@gmail.com', '11111111111'),
+(3, 'José', 'exemploEmail3@gmail.com', '22222222222');
 
+INSERT INTO evento(id, nome, descricao, local, data) VALUES
+(1, 'EventoA', 'descricao do evento a', 'patos de minas', '2025-09-03'),
+(2, 'EventoB', 'descricao do evento b', 'patos de minas', '2025-08-04');
+
+INSERT INTO inscricao(id, data, status, id_evento, id_participante) VALUES
+(1,'2025-09-03', 'realizada', 1,1),
+(2,'2025-09-02', 'pendente', 1,2),
+(3,'2025-09-01', 'realizada', 2,2),
+(4,'2025-09-28', 'pendente', 2,3);
+
+INSERT INTO pagamento(id, valor, data, status, id_inscricao) VALUES
+(1, 100, '2025-09-05', 'pago', 1),
+(2, 90, '2025-09-06', 'pendente', 2),
+(3, 80, '2025-09-07', 'pago', 3),
+(4, 70, '2025-09-08', 'pendente', 4);
